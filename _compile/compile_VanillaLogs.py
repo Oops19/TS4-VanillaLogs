a8241cec43dedac49bcee898d38f065dc22a3e20
Oops19 <Oops19@users.noreply.github.com>
2024-02-03 16:50:15 +0100

    update

--- _compile/compile_VanillaLogs.py
+++ _compile/compile_VanillaLogs.py
@@ -1,4 +1,4 @@
-# compile.sh version 2.0.12
+# compile.sh version 2.0.15
 
 # This file searches from the parent directory for 'modinfo.py' in it or in any sub directory.
 # Make sure to have only one 'modinfo.py' in your project directory. The first found 'modinfo.py' is used and loaded.
@@ -22,12 +22,12 @@ from typing import Tuple, Dict, Any
 
 from Utilities.unpyc3_compiler import Unpyc3PythonCompiler
 
-
 additional_directories: Tuple = ()
 include_sources = False
 exclude_folders: Tuple = ()
 add_readme = True
 file_appendix = ''
+auto_beta = True
 try:
     with open('compile.ini', 'rt') as fp:
         cfg: Dict[str, Any] = ast.literal_eval(fp.read())
@@ -36,6 +36,7 @@ try:
         exclude_folders = cfg.get('exclude_folders', exclude_folders)
         add_readme = cfg.get('add_readme', add_readme)
         file_appendix = cfg.get('file_appendix', file_appendix)
+        auto_beta = cfg.get('auto_beta', auto_beta)
 except:
     pass
 
@@ -44,6 +45,8 @@ beta_appendix = "-beta"  # or "-test-build"
 modinfo_py = 'modinfo.py'
 mi = None
 for root, dirs, files in os.walk('..'):
+    if '.private' in root:
+        continue
     if modinfo_py in files:
         modinfo = os.path.join(root, modinfo_py)
         print(f"Using '{modinfo}' ...")
@@ -51,8 +54,10 @@ for root, dirs, files in os.walk('..'):
             sys.path.insert(1, root)
             # noinspection PyUnresolvedReferences
             from modinfo import ModInfo
+
             mi = ModInfo.get()
-            print(f"Imported data for '{mi._author}:{mi._name}' from '../{mi._base_namespace}' with version '{mi._version}'")
+            print(
+                f"Imported data for '{mi._author}:{mi._name}' from '../{mi._base_namespace}' with version '{mi._version}'")
             break
         except Exception as e:
             print(f"Error importing '{modinfo_py}' ({e}).")
@@ -68,6 +73,12 @@ mod_directory = mi._base_namespace
 version = mi._version  # All versions 0., x.1, x.3, x.5, x.7, x.9 (also x.1.y, x.1.y.z) will be considered beta and the 'beta_appendix' gets appended.
 
 if add_readme:
+    file_game_version = 'c:' + os.sep + os.path.join(os.environ['HOMEPATH'], 'Documents', 'Electronic Arts',
+                                                     'The Sims 4', 'GameVersion.txt')
+    with open(file_game_version, 'rb') as fp:
+        _game_version = fp.read()
+        game_version = _game_version[4:].rsplit(b'.', 1)[0].decode('ASCII')
+
     file_readme = os.path.join('..', '.private', 'README.md')
     file_footer = os.path.join('..', '..', 'FOOTER.md')
     gitignore = os.path.join('..', '.gitignore')
@@ -79,9 +90,11 @@ if add_readme:
             with open(file_w, 'wb') as fp_w:
                 for file_r in [file_readme, file_footer]:
                     with open(file_r, 'rb') as fp_r:
-                        fp_w.write(fp_r.read())
+                        _data = fp_r.read()
+                        data = re.sub(r'1.103.315 .2023-12.', game_version, _data.decode('UTF-8'))
+                        fp_w.write(data.encode('UTF-8'))
         with open(gitignore, 'rt') as fp:
-            if not ".private" in fp.read():
+            if ".private" not in fp.read():
                 fp.close()
                 with open(gitignore, 'at', newline='\n') as fp:
                     fp.write('\n#Private data\n.private\n')
@@ -91,8 +104,13 @@ if add_readme:
 
 release_directory = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(os.getcwd()))), 'Release')
 mod_base_directory = os.path.join(release_directory, mod_name)
-ts4_directory = os.path.join(mod_base_directory, 'Mods', f"_{author}_")
+mod_base_directory_backup = os.path.join(release_directory, f"{mod_name}.bak")
+if os.path.exists(mod_base_directory_backup):
+    shutil.rmtree(mod_base_directory_backup)
+if os.path.exists(mod_base_directory):
+    os.rename(mod_base_directory, mod_base_directory_backup)
 
+ts4_directory = os.path.join(mod_base_directory, 'Mods', f"_{author}_")
 
 src_folder = os.path.join(os.path.dirname(os.path.abspath(os.getcwd())), '_TS4')
 for folder in ['mod_data', 'mod_documentation', 'Mods', 'mod_sources']:
@@ -109,26 +127,30 @@ for folder in ['mod_data', 'mod_documentation', 'Mods', 'mod_sources']:
 zip_file_name = os.path.join(release_directory, f"{mod_name}")
 if version:
     zip_file_name = f"{zip_file_name}_v{version}"
-    if re.match(r"^(?:0|(?:0|[1-9][0-9]*)\.[0-9]*[13579])(?:\.[0-9]+)*$", version):
-        zip_file_name = f"{zip_file_name}{beta_appendix}"
+    if auto_beta:
+        if re.match(r"^(?:0|(?:0|[1-9][0-9]*)\.[0-9]*[13579])(?:\.[0-9]+)*$", version):
+            zip_file_name = f"{zip_file_name}{beta_appendix}"
 zip_file_name = f"{zip_file_name}{file_appendix}"
 
 # Add source
 if include_sources:
     _mod_src_directory = os.path.dirname(os.path.abspath(os.getcwd()))
-    for folder in (mod_directory, ) + additional_directories:
+    for folder in (mod_directory,) + additional_directories:
         try:
-            shutil.copytree(os.path.join(_mod_src_directory, folder), os.path.join(mod_base_directory, 'mod_sources', mod_name, folder), ignore=shutil.ignore_patterns('__pycache__', '.*'))
+            shutil.copytree(os.path.join(_mod_src_directory, folder),
+                            os.path.join(mod_base_directory, 'mod_sources', mod_name, folder),
+                            ignore=shutil.ignore_patterns('__pycache__', '.*'))
         except Exception as e:
             print(f"{e}")
-            print(f"WARNING: Remove the folder {os.path.join(mod_base_directory, 'mod_sources', mod_name, folder)} to update the data.")
+            print(
+                f"WARNING: Remove the folder {os.path.join(mod_base_directory, 'mod_sources', mod_name, folder)} to update the data.")
 
 # Compile
 os.makedirs(ts4_directory, exist_ok=True)
 print(f"Compiling '{mod_directory}' and {additional_directories} in '{ts4_directory}'")
 
 Unpyc3PythonCompiler.compile_mod(
-    names_of_modules_include=(mod_directory, ) + additional_directories,
+    names_of_modules_include=(mod_directory,) + additional_directories,
     folder_path_to_output_ts4script_to=ts4_directory,
     output_ts4script_name=mod_directory
 )
@@ -145,10 +167,18 @@ shutil.make_archive(os.path.join(release_directory, f"{zip_file_name}"), 'zip', 
 print(f'Created {os.path.join(release_directory, f"{zip_file_name}.zip")}')
 
 '''
+v2.0.15
+    Make a backup of the existing directory and create a full/new build
+v2.0.14
+    read game-version from game_version.txt
+v2.0.13
+    Exclude .private/modinfo.py
+    compile.ini options:
+        'auto_beta': True,  - Set to False to avoid '-beta' naming for the ZIP file
 v2.0.12
     compile.ini options:
-        'add_readme': True,
-        'file_appendix': ''
+        'add_readme': True,  - Set to False to avoid creating README.md
+        'file_appendix': ''  - Define a custom "-xxx" appendix for the ZIP file
 v2.0.11
     Fixed exclude_folders
 v2.0.10
